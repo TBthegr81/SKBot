c8f248ca465936e6cf461d29447fa86dad3a9544
TB <TB@WIN-MIED1IEVT35>
2014-11-09 13:20:08 +0100

    yeaupp

--- src/botMk2/Commands/Random.java
+++ src/botMk2/Commands/Random.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import botMk2.Lib;
 
 public class Random implements botMk2.Command {
+	String apikey = "1234567890";
+	String returntype = "HTML";
 	private String[] triggers = {""};
 	public ArrayList<String> evaluate(String[] input) {
 		//String tag = "";
@@ -17,10 +19,9 @@ public class Random implements botMk2.Command {
 				//Check if there could be some other inputs to the function
 				if(input.length > 1)
 				{
-					String apikey = "1234567890";
+					
 					//String datatype = "randomLink";
 					String[] tags = {"bajs"};
-					String returntype = "HTML";
 					String[] result = Lib.readWebsite("http://skbot.snekabel.se/api/v0/getRandomLink.php?apikey="+apikey+"&tags="+tags[0]+"&returntype="+returntype);
 					for(int i = 0; i < result.length; i++)
 					{
@@ -30,7 +31,11 @@ public class Random implements botMk2.Command {
 				// Else just go with the default output
 				else
 				{
-					answer.add("Shadap i dont care");
+					String[] result = Lib.readWebsite("http://skbot.snekabel.se/api/v0/getRandomLink.php?apikey="+apikey+"&returntype="+returntype);
+					for(int i = 0; i < result.length; i++)
+					{
+						System.out.println(result[i]);
+					}
 				}
 			}
 		}
