1a6ade887176639633a04e096293ff53622ce471
TBthegr81 <tbthegr81@gmail.com>
2016-08-03 20:31:53 +0200

    Fixed links, moved away from Kira-stuff, now using JSOUP for Web-parsning

--- src/botMk3/Commands/link.java
+++ src/botMk3/Commands/link.java
@@ -2,7 +2,12 @@ package botMk3.Commands;
 
 import botMk3.Interfaces.Command;
 import botMk3.Lib;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class link implements Command {
@@ -11,15 +16,33 @@ public class link implements Command {
     public ArrayList<String> evaluate(String[] input) {
         //String tag = "";
         ArrayList<String> answer = new ArrayList<String>();
-	
-	String title = "Title: ";
+
 	for(int i = 0; i < input.length; i++)
 	{
+        String answertext = "Title: ";
 		if(input[i].toLowerCase().contains("http://") || input[i].toLowerCase().contains("https://"))
 		{
-            Lib.readWebsite(input[i]);
-			title += "google.se - Search things";
-			answer.add(title);
+            //Lib.readWebsite(input[i]);
+			//title += "google.se - Search things";
+
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(input[i]).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String title = "";
+            Elements titles = doc.select("title");
+            for (Element titletext : titles)
+            {
+                title += titletext.text();
+            }
+            System.out.println(title);
+            answertext += title;
+            if(title != null && !title.isEmpty())
+            {
+                answer.add(answertext);
+            }
 		}
 	}
 	 return answer;
